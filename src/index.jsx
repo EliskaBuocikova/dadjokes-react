8d1d9f196d150d0d5e48ef9cb8f4c6c915bc454e
Eliška Buociková <buocikovaeliska@gmail.com>
2023-06-03 08:35:25 +0200

    základní struktura aplikace, oživení tlačítek like a disslike

---
+++ src/index.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { createRoot } from 'react-dom/client';
+import './style.css';
+
+const App = () => {
+  const [like, setLike] = useState(0);
+  const [disslike, setDisslike] = useState(0);
+
+  const handleClickLike = () => {
+    setLike(like + 1);
+  };
+
+  const handleClickDisslike = () => {
+    setDisslike(disslike + 1);
+  };
+
+  return (
+    <div className="container">
+      <div className="joke">
+        <div className="joke__body">
+          <div className="joke__user">
+            <img
+              className="user-avatar"
+              src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
+            />
+            <p className="user-name">Neroxx</p>
+          </div>
+
+          <p className="joke__text">
+            The secret service isn't allowed to yell "Get down!" anymore when
+            the president is about to be attacked. Now they have to yell
+            "Donald, duck!"
+          </p>
+        </div>
+        <div className="joke__likes">
+          <button
+            onClick={handleClickLike}
+            id="btn-up"
+            className="btn-like btn-like--up"
+          ></button>
+          <span id="likes-up" className="likes-count likes-count--up">
+            {like}
+          </span>
+          <button
+            onClick={handleClickDisslike}
+            id="btn-down"
+            className="btn-like btn-like--down"
+          ></button>
+          <span id="likes-down" className="likes-count likes-count--down">
+            {disslike}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+createRoot(document.querySelector('#app')).render(<App />);
